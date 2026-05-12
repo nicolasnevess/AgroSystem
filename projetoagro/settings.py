63d928bffc7fe934929888e6c4e3b382a3e6feea
NICOLAS NEVES ALTARUJO <nicolas.altarujo@domboscopira.com.br>
2026-05-12 08:10:42 -0300

    Ajuste variável

--- projetoagro/settings.py
+++ projetoagro/settings.py
@@ -150,9 +150,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nicaogamer098@gmail.com'
-EMAIL_HOST_PASSWORD = 'wadyohduwrlluhka'
-DEFAULT_FROM_EMAIL = 'AgroSystem <nicaogamer098@gmail.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = f'AgroSystem <{os.getenv("EMAIL_USER")}>'
 
 # FOTOS
 
